b9b6361aa0e6424f03d97bec43f8bd073a4c1014
MuhammadSamiMasood <samimasood88@gmail.com>
2019-05-16 02:21:02 -0700

    Now getting the server URL through config.json file, and posting single Shape object through post request instead of complete Shape arrayList.

--- src/main/java/com/ebricks/shape/processor/ShapeProcessor.java
+++ src/main/java/com/ebricks/shape/processor/ShapeProcessor.java
@@ -7,11 +7,7 @@ import com.ebricks.shape.model.Shape;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import javax.print.DocFlavor;
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
@@ -23,26 +19,16 @@ public class ShapeProcessor {
     private Canvas canvasReader = null;
     private ExecutorService service = null;
     List<Future> futures = null;
-    String input = "";
-    String output;
+    String shapesJson = "";
+    String shapeJsonToPost;
+    ShapeService shapeService = new ShapeService();
 
     public void init() throws IOException {
 
-        URL url = new URL("http://localhost:8080/shapeserver/Shapes");
-        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-        httpURLConnection.setRequestMethod("GET");
-        httpURLConnection.setRequestProperty("User-Agent", "Mozilla/5.0");
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-        String inputLine;
-        while ((inputLine = bufferedReader.readLine()) != null) {
-            input += inputLine;
-        }
-        bufferedReader.close();
-        //httpURLConnection.disconnect();
+        shapesJson = (String)shapeService.get();
 
         objectMapper = new ObjectMapper();
-        canvasReader = (Canvas) objectMapper.readValue(input, Canvas.class);
+        canvasReader = (Canvas) objectMapper.readValue(shapesJson, Canvas.class);
 
         service = Executors.newFixedThreadPool(2);
 
@@ -62,42 +48,10 @@ public class ShapeProcessor {
         }
 
         Shape randomShape = new Circle(50.4);
-        canvasReader.getShapes().add(randomShape);
-        output = objectMapper.writeValueAsString(canvasReader);
-        logger.info("\n" + output);
+        shapeJsonToPost = objectMapper.writeValueAsString(randomShape);
 
-        URL url = new URL("http://localhost:8080/shapeserver/Shapes");
-        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-
-        httpURLConnection.setRequestMethod("POST");
-        httpURLConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-        //httpURLConnection.connect();
-
-        httpURLConnection.setDoOutput(true);
-        DataOutputStream dataOutputStream = new DataOutputStream(httpURLConnection.getOutputStream());
-        dataOutputStream.writeBytes(output);
-        dataOutputStream.flush();
-        dataOutputStream.close();
-
-        int responseCode = httpURLConnection.getResponseCode();
-        System.out.println("POST Response Code :: " + responseCode);
-        if (responseCode == HttpURLConnection.HTTP_OK) { //success
-            BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            // print result
-            logger.info(response.toString());
-        } else {
-            System.out.println("POST request not worked");
-        }
-
-
+        String postResponse = (String)shapeService.post(shapeJsonToPost);
+        logger.info(postResponse);
     }
 
     public void end() throws InterruptedException {
